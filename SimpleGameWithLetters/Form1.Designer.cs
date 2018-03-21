9b6f569c00c2485bf645ec9b1054ec29f512ed3a
Hajk <tunczorx@gmail.com>
2018-03-21 13:31:16 +0100

    Simple but nice game with letters

--- SimpleGameWithLetters/Form1.Designer.cs
+++ SimpleGameWithLetters/Form1.Designer.cs
@@ -29,12 +29,136 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.correctLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.missedLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.totalLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.accuracyLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.difficultyProgressBar = new System.Windows.Forms.ToolStripProgressBar();
+            this.button1 = new System.Windows.Forms.Button();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 108;
+            this.listBox1.Location = new System.Drawing.Point(0, 0);
+            this.listBox1.MultiColumn = true;
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(856, 142);
+            this.listBox1.TabIndex = 0;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 800;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.correctLabel,
+            this.missedLabel,
+            this.totalLabel,
+            this.accuracyLabel,
+            this.statusLabel,
+            this.difficultyProgressBar});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 120);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(856, 22);
+            this.statusStrip1.SizingGrip = false;
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // correctLabel
+            // 
+            this.correctLabel.Name = "correctLabel";
+            this.correctLabel.Size = new System.Drawing.Size(93, 17);
+            this.correctLabel.Text = "Prawidłowych: 0";
+            // 
+            // missedLabel
+            // 
+            this.missedLabel.Name = "missedLabel";
+            this.missedLabel.Size = new System.Drawing.Size(58, 17);
+            this.missedLabel.Text = "Błędów: 0";
+            // 
+            // totalLabel
+            // 
+            this.totalLabel.Name = "totalLabel";
+            this.totalLabel.Size = new System.Drawing.Size(77, 17);
+            this.totalLabel.Text = "Wszystkich: 0";
+            // 
+            // accuracyLabel
+            // 
+            this.accuracyLabel.Name = "accuracyLabel";
+            this.accuracyLabel.Size = new System.Drawing.Size(91, 17);
+            this.accuracyLabel.Text = "Dokładność: 0%";
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(420, 17);
+            this.statusLabel.Spring = true;
+            this.statusLabel.Text = "Poziom trudności:";
+            this.statusLabel.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // difficultyProgressBar
+            // 
+            this.difficultyProgressBar.Maximum = 701;
+            this.difficultyProgressBar.Name = "difficultyProgressBar";
+            this.difficultyProgressBar.Size = new System.Drawing.Size(100, 16);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(702, 73);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(128, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Zagraj jeszcze raz!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Visible = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(856, 142);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.listBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.KeyPreview = true;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Gra w literki";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel correctLabel;
+        private System.Windows.Forms.ToolStripStatusLabel missedLabel;
+        private System.Windows.Forms.ToolStripStatusLabel totalLabel;
+        private System.Windows.Forms.ToolStripStatusLabel accuracyLabel;
+        private System.Windows.Forms.ToolStripStatusLabel statusLabel;
+        private System.Windows.Forms.ToolStripProgressBar difficultyProgressBar;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- SimpleGameWithLetters/Form1.cs
+++ SimpleGameWithLetters/Form1.cs
@@ -12,9 +12,71 @@ namespace SimpleGameWithLetters
 {
     public partial class Form1 : Form
     {
+        Random random = new Random();
+        Stats stats = new Stats();
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            // We add random letter to listBox1
+            listBox1.Items.Add((Keys)random.Next(65, 90));
+            if (listBox1.Items.Count > 7) // end game when in listbox we will have 8 or more letters
+            {
+                //ending game methods
+                listBox1.Items.Clear();
+                listBox1.Items.Add("Koniec gry");
+                timer1.Stop();
+                button1.Visible = true; //showing button which allows restart game
+            }
+        }
+
+        public void VisualStatisticsRestart()
+        {
+
+            correctLabel.Text = "Prawidłowych: " + stats.Correct;
+            missedLabel.Text = "Błędów: " + stats.Missed;
+            totalLabel.Text = "Wszystkich: " + stats.Total;
+            accuracyLabel.Text = "Dokładnośc: " + stats.Accuracy + "%";
+        }
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            // If player clicked letter which is showed in listbox, then delete it
+            // Difficulty ++
+            if (listBox1.Items.Contains(e.KeyCode))
+            {
+                listBox1.Items.Remove(e.KeyCode);
+                listBox1.Refresh();
+                if (timer1.Interval > 400)
+                    timer1.Interval -= 10;
+                if (timer1.Interval > 250)
+                    timer1.Interval -= 7;
+                if (timer1.Interval > 100)
+                    timer1.Interval -= 2;
+                difficultyProgressBar.Value = 800 - timer1.Interval;
+
+                stats.Update(true); //correctKey
+            }
+            else
+            {
+                stats.Update(false); //!correctKey
+            }
+
+            VisualStatisticsRestart();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Visible = false;
+            listBox1.Items.Clear();
+            stats.Restart();
+            VisualStatisticsRestart();
+            difficultyProgressBar.Value = 0; //restarting progress bar
+            timer1.Interval = 701; //setting timer1 interval to default
+            timer1.Start(); //starting game
         }
     }
 }
